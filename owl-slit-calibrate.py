da45dd6b643905d6af266a391d78e96e1d87af9e
William Henney <will@henney.org>
2016-08-17 12:30:12 -0500

    First version of isovelocity images
    Loads of changes

---
+++ owl-slit-calibrate.py
@@ -0,0 +1,173 @@
+# [[file:teresa-owl.org::*Program%20owl-slit-calibrate.py][Program\ owl-slit-calibrate\.py:1]]
+import os
+import sys
+import numpy as np
+import astropy
+from astropy.table import Table, hstack
+from astropy.io import fits
+from astropy.wcs import WCS
+from astropy.wcs.utils import pixel_to_skycoord
+from matplotlib import pyplot as plt
+import seaborn as sns
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+from astropy.modeling import models, fitting
+from owl_utils import (DATADIR, slit_profile, extract_profile, 
+                       find_slit_coords, make_slit_wcs, remove_bg_and_regularize)
+
+restwavs = {'ha': 6562.79, 'nii': 6583.45}
+
+def fit_cheb(x, y, npoly=3, mask=None):
+    """Fits a Chebyshev poly to y(x) and returns fitted y-values"""
+    fitter = fitting.LinearLSQFitter()
+    p_init = models.Chebyshev1D(npoly, domain=[x.min(), x.max()])
+    if mask is None:
+        mask = np.ones_like(x).astype(bool)
+    p = fitter(p_init, x[mask], y[mask])
+    print(p)
+    return p(x)
+
+sns.set_palette('RdPu_d', 3)
+def make_three_plots(spec, calib, prefix, niirat=None):
+    assert spec.shape == calib.shape
+    fig, axes = plt.subplots(3, 1)
+
+
+    ypix = np.arange(len(calib))
+    m = np.isfinite(spec) & np.isfinite(calib)
+    spec = spec[m]
+    calib = calib[m]
+    ypix = ypix[m]
+    if niirat is not None:
+        niirat = niirat[m]
+
+    vmin, vmax = 0.0, np.percentile(calib, 95) + 4*calib.std()
+    ratio = spec/calib
+    mask = (spec > np.percentile(spec, 25)) & (ratio > 0.5) & (ratio < 2.0)
+    # mask = (ypix > 10.0) & (ypix < ypix.max() - 10.0) \
+    #        & (ratio > np.median(ratio) - 2*ratio.std()) \
+    #        & (ratio < np.median(ratio) + 2*ratio.std()) 
+    try:
+        ratio_fit = fit_cheb(ypix, ratio, mask=mask, npoly=1)
+    except:
+        ratio_fit = np.ones_like(ypix)
+
+    alpha = 0.8
+
+    # First, plot two profiles against each other to check for zero-point offsets
+    axes[0].plot(calib, spec/ratio_fit, '.', alpha=alpha)
+    axes[0].plot([vmin, vmax], [vmin, vmax], '-', alpha=alpha)
+    axes[0].set_xlim(vmin, vmax)
+    axes[0].set_ylim(vmin, vmax)
+    axes[0].set_xlabel('Calibration Image')
+    axes[0].set_ylabel('Corrected Integrated Spectrum')
+
+    # Second, plot each against slit pixel to check spatial offset
+    axes[1].plot(ypix, calib, alpha=alpha, label='Calibration Image')
+    axes[1].plot(ypix, spec/ratio_fit, alpha=alpha, lw=1.0,
+                 label='Corrected Integrated Spectrum')
+    axes[1].plot(ypix, spec, alpha=alpha, lw=0.5,
+                 label='Uncorrected Integrated Spectrum')
+    axes[1].set_xlim(0.0, ypix.max())
+    axes[1].set_ylim(vmin, vmax)
+    axes[1].legend(fontsize='xx-small', loc='upper right')
+    axes[1].set_xlabel('Slit pixel')
+    axes[1].set_ylabel('Profile')
+
+    # Third, plot ratio to look for spatial trends
+    axes[2].plot(ypix, ratio, alpha=alpha)
+    axes[2].plot(ypix, ratio_fit, alpha=alpha)
+    if niirat is not None:
+        axes[2].plot(ypix, niirat, 'b', lw=0.5, alpha=0.5)
+    axes[2].set_xlim(0.0, ypix.max())
+    axes[2].set_ylim(0.0, 2.5)
+    axes[2].set_xlabel('Slit pixel')
+    axes[2].set_ylabel('Ratio: Spec / Calib')
+
+
+    fig.set_size_inches(5, 8)
+    fig.tight_layout()
+    fig.savefig(prefix+'.png', dpi=300)
+
+    return ratio_fit
+
+
+hatab = Table.read('spectra-ha.tab', format='ascii.tab')
+niitab = Table.read('spectra-nii.tab', format='ascii.tab')
+imtab = Table.read('list-of-images.tab', format='ascii.tab')
+slittab = Table.read('slit-positions.tab', format='ascii.tab')
+table = hstack([hatab, niitab, imtab, slittab],
+               table_names=['ha', 'nii', 'im', 'slit'])
+# Photometric reference image
+photom, = fits.open('imslit-median.fits')
+wphot = WCS(photom.header)
+
+for row in table:
+    ha_hdu, = fits.open(DATADIR +'/SPMha/' + row['file_ha'])
+    nii_hdu, = fits.open(DATADIR +'/SPMnii/' + row['file_nii'])
+    im_hdu, = fits.open(DATADIR +'/' + row['filename'])
+    im_hdu.header.remove('@EPOCH')
+
+    slit_coords = find_slit_coords(row, im_hdu.header, ha_hdu.header)
+    calib_profile = slit_profile(slit_coords['RA'], slit_coords['Dec'],
+                                 photom.data, wphot)
+
+    ha_profile, ha_bg = extract_profile(ha_hdu.data, WCS(ha_hdu.header),
+                                        restwavs['ha'], row)
+    nii_profile, nii_bg = extract_profile(nii_hdu.data, WCS(nii_hdu.header),
+                                          restwavs['nii'], row)
+
+
+    # Make a fake Ha+[N II] line (really should add continuum too)
+    spec_profile = (ha_profile+1.333*nii_profile) + 0.05*(ha_bg + 2*nii_bg)
+    # Zeroth order approximation to calibration
+    rat0 = np.nansum(spec_profile)/np.nansum(calib_profile)
+    print('Coarse calibration: ratio =', rat0)
+    spec_profile /= rat0
+
+    plt_prefix = '{:03d}-calib'.format(row.index+1)
+    ratio = make_three_plots(spec_profile, calib_profile,
+                             plt_prefix, niirat=nii_profile/ha_profile)
+
+    # Save calibrated spectra to files
+    for lineid, hdu in [['ha', ha_hdu], ['nii', nii_hdu]]:
+        restwav = restwavs[lineid]
+        print('Saving', lineid, 'calibrated spectrum')
+        # Apply basic calibration zero-point and scale
+        hdu.data, _ = remove_bg_and_regularize(hdu.data, WCS(hdu.header),
+                                               restwav, row)/rat0
+        # Regularize spectral data so that wavelength is x and pos is y
+        # This is now done by the bg subtraction function
+
+        # Apply polynomial correction along slit
+        if ratio is not None:
+            hdu.data /= ratio.mean()
+        # Extend in the third dimension (degenerate axis perp to slit)
+        hdu.data = hdu.data[None, :, :]
+
+        # Create the WCS object for the calibrated slit spectra
+        wslit = make_slit_wcs(row, slit_coords, hdu)
+        # Set the rest wavelength for this line
+        wslit.wcs.restwav = (restwav*u.Angstrom).to(u.m).value
+        # # Remove WCS keywords that might cause problems
+        # for i in 1, 2:
+        #     for j in 1, 2:
+        #         kwd = 'CD{}_{}'.format(i, j)
+        #         if kwd in hdu.header:
+        #             hdu.header.remove(kwd) 
+        # Then update the header with the new WCS structure as the 'A'
+        # alternate transform
+        hdu.header.update(wslit.to_header(key='A'))
+        # Also save the normalization factor as a per-slit weight to use later
+        hdu.header['WEIGHT'] = rat0
+
+        # And better not to change the original WCS at all
+        # Unless we have transposed the array, which we have to compensate for
+        if row['saxis'] == 1:
+            for k in ['CRPIX{}', 'CRVAL{}', 'CDELT{}', 'CD{0}_{0}']:
+                hdu.header[k.format('1')], hdu.header[k.format('2')] = hdu.header[k.format('2')], hdu.header[k.format('1')] 
+        # # And write a bowdlerized version that DS9 can understand as the main WCS
+        # hdu.header.update(fixup4ds9(wslit).to_header(key=' '))
+        calibfile = 'Calibrated/{}-{}.fits'.format(row['pos_nii'], lineid)
+        hdu.writeto(calibfile, clobber=True)
+# Program\ owl-slit-calibrate\.py:1 ends here
--- owl_utils.py
+++ owl_utils.py
@@ -1,1 +1,213 @@
+import numpy as np
+from astropy.wcs import WCS
+from astropy.coordinates import SkyCoord
+from astropy.wcs.utils import pixel_to_skycoord
+from astropy import units as u
+
 DATADIR = '/Users/will/Dropbox/Papers/LL-Objects/NGC3587'
+
+def slit_profile(ra, dec, image, wcs):
+    """
+    Find the image intensity for a list of positions (ra and dec)
+    """
+    xi, yj = wcs.all_world2pix(ra, dec, 0)
+    # Find nearest integer pixel
+    ii, jj = np.floor(xi + 0.5), np.floor(yj + 0.5)
+    print(ra[::100], dec[::100])
+    print(ii[::100], jj[::100])
+    ny, nx = image.shape
+    return np.array([image[j, i] if (i < nx and j < ny) else np.nan for i, j in zip(ii, jj)])
+
+def find_slit_coords(db, hdr, shdr):
+    """Find the coordinates of all the pixels along a spectrograph slit
+
+    Input arguments are a dict-like 'db' of hand-measured values (must
+    contain 'saxis', 'islit' and 'shift') and a FITS headers 'hdr' from
+    the image+slit exposure and 'shdr' from a spectrum exposure
+
+    Returns a dict of 'ds' (slit pixel scale), 'PA' (slit position
+    angle), 'RA' (array of RA values in degrees along slit), 'Dec'
+    (array of Dec values in degrees along slit)
+
+    """
+    jstring = str(db['saxis'])  # which image axis lies along slit
+    dRA_arcsec = hdr['CD1_'+jstring]*3600*np.cos(np.radians(hdr['CRVAL2']))
+    dDEC_arcsec = hdr['CD2_'+jstring]*3600
+    ds = np.hypot(dRA_arcsec, dDEC_arcsec)
+    PA = np.degrees(np.arctan2(dRA_arcsec, dDEC_arcsec))
+
+    # Pixel coords of each slit pixel on image (in 0-based convention)
+    if jstring == '1':
+        # Slit is horizontal in IMAGE coords
+        ns = shdr['NAXIS1']
+        iarr = np.arange(ns) - float(db['shift'])
+        jarr = np.ones(ns)*float(db['islit'])
+        try:
+            image_binning = hdr['CBIN']
+            spec_binning = shdr['CBIN']
+        except KeyError:
+            image_binning = hdr['CCDXBIN']
+            spec_binning = shdr['CCDXBIN']
+
+        # correct for difference in binning between the image+slit and the spectrum
+        iarr *= spec_binning/image_binning
+    elif jstring == '2':
+        # Slit is vertical in IMAGE coords
+        ns = shdr['NAXIS2']
+        iarr = np.ones(ns)*float(db['islit'])
+        jarr = np.arange(ns) - float(db['shift'])
+        try:
+            image_binning = hdr['RBIN']
+            spec_binning = shdr['RBIN']
+        except KeyError:
+            image_binning = hdr['CCDYBIN']
+            spec_binning = shdr['CCDYBIN']
+
+        jarr *= spec_binning/image_binning
+    else:
+        raise ValueError('Slit axis (saxis) must be 1 or 2')
+
+    print('iarr =', iarr[::100], 'jarr =', jarr[::100])
+    # Also correct the nominal slit plate scale
+    ds *= spec_binning/image_binning
+
+    # Convert to world coords, using the native frame
+    w = WCS(hdr)
+    observed_frame = w.wcs.radesys.lower()
+    # Note it is vital to ensure the pix2world transformation returns
+    # values in the order (RA, Dec), even if the image+slit may have
+    # Dec first
+    coords = SkyCoord(*w.all_pix2world(iarr, jarr, 0, ra_dec_order=True),
+                      unit=(u.deg, u.deg), frame=observed_frame)
+    print('coords =', coords[::100])
+    print('Binning along slit: image =', image_binning, 'spectrum =', spec_binning)
+    # Make sure to return the coords in the ICRS frame
+    return {'ds': ds, 'PA': PA,
+            'RA': coords.icrs.ra.value,
+            'Dec': coords.icrs.dec.value}
+
+def make_slit_wcs(db, slit_coords, spechdu):
+    # Input WCS from original spectrum
+    wspec = WCS(spechdu.header)
+    wspec.fix()
+
+    #
+    # First find wavelength scale from the spectrum  
+    #
+
+    # For original spectrum, the wavelength and slit axes are 0-based,
+    # but in FITS axis order instead of python access order, since
+    # that is the way that that the WCS object likes to do it
+    ospec_wavaxis = 2 - db['saxis']
+    ospec_slitaxis = db['saxis'] - 1
+
+    # The rules are that CDi_j is used if it is present, and only if
+    # it is absent should CDELTi be used
+    if wspec.wcs.has_cd():
+        dwav = wspec.wcs.cd[ospec_wavaxis, ospec_wavaxis]
+        # Check that the off-diagonal terms are zero
+        assert(wspec.wcs.cd[0, 1] == wspec.wcs.cd[1, 0] == 0.0)
+    else:
+        dwav = wspec.wcs.cdelt[ospec_wavaxis]
+        if wspec.wcs.has_pc():
+            # If PCi_j is also present, make sure it is identity matrix
+            assert(wspec.wcs.pc == np.eye(2))
+    wav0 = wspec.wcs.crval[ospec_wavaxis]
+    wavpix0 = wspec.wcs.crpix[ospec_wavaxis]
+
+    #
+    # Second, find the displacement scale and ref point from the slit_coords
+    #
+    # The slit_coords should already be in ICRS frame
+    c = SkyCoord(slit_coords['RA'], slit_coords['Dec'], unit=u.deg)
+    # Find vector of separations between adjacent pixels
+    seps = c[:-1].separation(c[1:])
+    # Ditto for the position angles
+    PAs = c[:-1].position_angle(c[1:])
+    # Check that they are all the same as the first one
+    assert(np.allclose(seps/seps[0], 1.0))
+    # assert(np.allclose(PAs/PAs[0], 1.0, rtol=1.e-4))
+    # Then use the first one as the slit pixel size and PA
+    ds, PA, PA_deg = seps[0].deg, PAs.mean().rad, PAs.mean().deg
+    # And for the reference values too
+    RA0, Dec0 = c[0].ra.deg, c[0].dec.deg
+
+    #
+    # Now make a new shiny output WCS, constructed from scratch
+    #
+    w = WCS(naxis=3)
+
+    # Make use of all the values that we calculated above
+    w.wcs.crpix = [wavpix0, 1, 1]
+    w.wcs.cdelt = [dwav, ds, ds]
+    w.wcs.crval = [wav0, RA0, Dec0]
+    # PC order is i_j = [[1_1, 1_2, 1_3], [2_1, 2_2, 2_3], [3_1, 3_2, 3_3]]
+    w.wcs.pc = [[1.0, 0.0, 0.0],
+                [0.0, np.sin(PA), -np.cos(PA)],
+                [0.0, np.cos(PA), np.sin(PA)]]
+
+    #
+    # Finally add in auxillary info
+    #
+    w.wcs.radesys = 'ICRS'
+    w.wcs.ctype = ['AWAV', 'RA---TAN', 'DEC--TAN']
+    w.wcs.specsys = 'TOPOCENT'
+    w.wcs.cunit = [u.Angstrom, u.deg, u.deg]
+    w.wcs.name = 'TopoWav'
+    w.wcs.cname = ['Observed air wavelength', 'Right Ascension', 'Declination']
+    w.wcs.mjdobs = wspec.wcs.mjdobs
+    w.wcs.datfix()              # Sets DATE-OBS from MJD-OBS
+
+    # Check the new pixel values
+    npix = len(slit_coords['RA'])
+    check_coords = pixel_to_skycoord(np.arange(npix), [0]*npix, w, 0)
+    # These should be the same as the ICRS coords in slit_coords
+    print('New coords:', check_coords[::100])
+    print('Displacements in arcsec:', check_coords.separation(c).arcsec[::100])
+    # 15 Sep 2015: They seem to be equal to within about 1e-2 arcsec
+
+    return w
+
+def extract_profile(data, wcs, wavrest, db, dw=7.0):
+    data, bgdata = remove_bg_and_regularize(data, wcs, wavrest, db)
+    # pixel limits for line extraction
+    lineslice = wavs2slice([wavrest-dw/2, wavrest+dw/2], wcs, db)
+    return data[:, lineslice].sum(axis=1), bgdata.sum(axis=1)
+
+
+def wavs2slice(wavs, wcs, db):
+    """Convert a wavelength interval `wavs` (length-2 sequence) to a slice of the relevant axis`"""
+    assert len(wavs) == 2
+    isT = db['saxis'] == 1
+    if isT:
+        _, xpixels = wcs.all_world2pix([0, 0], wavs, 0)
+    else:
+        xpixels, _ = wcs.all_world2pix(wavs, [0, 0], 0)
+    print('Wav:', wavs, 'Pixel:', xpixels)
+    i1, i2 = np.maximum(0, (xpixels+0.5).astype(int))
+    return slice(min(i1, i2), max(i1, i2))
+
+
+def remove_bg_and_regularize(data, wcs, wavrest, db, dwbg_in=7.0, dwbg_out=10.0):
+    '''
+    Transpose data if necessary, and then subtract off the background (blue and red of line)
+    '''
+    isT = db['saxis'] == 1
+    # Make sure array axis order is (position, wavelength)
+    if isT:
+        data = data.T
+    # pixel limits for blue, red bg extraction
+    bslice = wavs2slice([wavrest-dwbg_out/2, wavrest-dwbg_in/2], wcs, db)
+    rslice = wavs2slice([wavrest+dwbg_in/2, wavrest+dwbg_out/2], wcs, db)
+    # extract backgrounds on blue and red sides
+    bgblu = data[:, bslice].mean(axis=1)
+    bgred = data[:, rslice].mean(axis=1)
+    # take weighted average, accounting for cases where the bg region
+    # does not fit in the image
+    weight_blu = data[:, bslice].size
+    weight_red = data[:, rslice].size
+    print('Background weights:', weight_blu, weight_red)
+    bg = (bgblu*weight_blu + bgred*weight_red)/(weight_blu + weight_red)
+    bgdata = np.zeros_like(data)
+    bgdata += bg[:, None]
+    return data - bgdata, bgdata
